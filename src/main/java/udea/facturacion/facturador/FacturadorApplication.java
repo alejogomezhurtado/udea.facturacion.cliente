585139ae0c3ab102716e68adbb69a7a65980ae40
alejandro gomez hurtado <alejo.gomez.h@gmail.com>
2018-08-15 20:35:54 -0500

    Proyecto final diplomado microservicios nivel 1
    * Commit inicial

---
+++ src/main/java/udea/facturacion/facturador/FacturadorApplication.java
@@ -0,0 +1,11 @@
+package udea.facturacion.facturador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturadorApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FacturadorApplication.class, args);
+	}
+}
---
+++ src/main/java/udea/facturacion/facturador/controllers/Cliente.java
@@ -0,0 +1,28 @@
+package udea.facturacion.facturador.controllers;
+
+import com.google.gson.Gson;
+import udea.facturacion.facturador.modelo.DtoCliente;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cliente {
+
+    private static Map<Integer, DtoCliente> listaClientes = new HashMap<>();
+
+    public static void crear(String message){
+        Gson gson = new Gson();
+        DtoCliente cliente = gson.fromJson(message, DtoCliente.class);
+        listaClientes.put(cliente.getIdCliente(), cliente);
+        System.out.println(cliente);
+    }
+
+    public static DtoCliente obtener(Integer id){
+        return listaClientes.get(id);
+    }
+
+    public static ArrayList<DtoCliente> lista(){
+        return new ArrayList<DtoCliente>(listaClientes.values());
+    }
+}
